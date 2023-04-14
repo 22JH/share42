76e8f35d73f6876ab609df8c21542917bb8e26a1
miracle3070 <miracle3070@naver.com>
2023-04-14 16:53:43 +0900

    [BE] ADD : Backend initial commit

---
+++ AMAG_Project/Backend/src/main/java/com/miracle/AMAG/AmagApplication.java
@@ -0,0 +1,13 @@
+package com.miracle.AMAG;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AmagApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AmagApplication.class, args);
+	}
+
+}
---
+++ AMAG_Project/Backend/src/test/java/com/miracle/AMAG/AmagApplicationTests.java
@@ -0,0 +1,13 @@
+package com.miracle.AMAG;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AmagApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

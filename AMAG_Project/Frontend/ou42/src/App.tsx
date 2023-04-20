330a0fded57d9a37095a40b2dbed4b624b700113
dlwngud9303 <dlwngud9303@naver.com>
2023-04-20 13:09:29 +0900

    [FE] ADD : react pwa typescript로 설정

---
+++ AMAG_Project/Frontend/ou42/src/App.tsx
@@ -0,0 +1,16 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./routes/Home";
+import NavBar from "./components/NavBar";
+
+function App() {
+  return (
+    <Router>
+      <NavBar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;

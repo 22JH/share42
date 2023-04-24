ca2c8aa7de942eecd6beb625f1bd54a477709cc3
ggnsall <ggnsall@naver.com>
2023-04-24 15:33:43 +0900

    [FE] ADD : #S08P31D102-182 관리자 홈 스켈레톤 코드 작성

---
+++ AMAG_Project/Frontend/ou42/src/components/admin/home/AdminHomeMenuBtn.tsx
@@ -0,0 +1,75 @@
+/** @jsxImportSource @emotion/react */
+import { css } from "@emotion/react";
+import { useNavigate } from "react-router-dom";
+
+import caution from "../../../assets/caution.svg";
+import logFile from "../../../assets/logFile.svg";
+import manufacturing from "../../../assets/manufacturing.svg";
+import map from "../../../assets/map.svg";
+
+const container = css`
+  .item-container {
+    display: flex;
+    flex-wrap: wrap;
+  }
+  .item {
+    display: flex;
+    align-items: center;
+    flex-direction: column;
+    justify-content: center;
+    flex: 1 1 30%;
+    height: 17vh;
+    background-color: rgba(245, 247, 254, 0.71);
+    border-radius: 30px;
+    color: #5f6065;
+    font-weight: 900;
+  }
+  .item:nth-of-type(2n) {
+    margin: 0 8% 2% 0;
+  }
+  .item:nth-of-type(2n + 1) {
+    margin: 0 2% 0 8%;
+  }
+`;
+
+const MENU_NUM = [
+  ["신고내역", caution, "/admin/report"],
+  ["로그조회", logFile, "/admin/report"],
+  ["기기조작", manufacturing, "/admin/report"],
+  ["지도", map, "/admin/report"],
+];
+
+function AdminHomeMenuBtn() {
+  const navigate = useNavigate();
+
+  return (
+    <div css={container}>
+      <div className="item-container">
+        {MENU_NUM.map((item, index) => {
+          return (
+            <div
+              key={`${index}/${item[1]}/${item[0]}`}
+              className="item"
+              onClick={() => navigate(`${item[2]}`)}
+            >
+              {/* <object
+                data={item[1]}
+                type="image/svg+xml"
+                style={{ marginTop: "13%" }}
+              ></object> */}
+              <img
+                src={item[1]}
+                style={{ marginTop: "13%" }}
+                onClick={() => navigate(`${item[2]}`)}
+              />
+
+              <p onClick={() => navigate(`${item[2]}`)}>{item[0]}</p>
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
+}
+
+export default AdminHomeMenuBtn;
---
+++ AMAG_Project/Frontend/ou42/src/routes/admin/AdminHome.tsx
@@ -0,0 +1,18 @@
+import AdminHomeChart from "../../components/admin/home/AdminHomeChart";
+import AdminHomeMenuBtn from "../../components/admin/home/AdminHomeMenuBtn";
+import AdminHomeTitle from "../../components/admin/home/AdminHomeTitle";
+
+function AdminHome() {
+  return (
+    <div style={{ width: "100vw", height: "90vh" }}>
+      {/* 타이틀 */}
+      <AdminHomeTitle />
+      {/* 차트 */}
+      <AdminHomeChart />
+      {/* 메뉴 버튼 */}
+      <AdminHomeMenuBtn />
+    </div>
+  );
+}
+
+export default AdminHome;

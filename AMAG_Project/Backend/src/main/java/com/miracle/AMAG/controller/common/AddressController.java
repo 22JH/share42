3790f90fbb02288c676334eb64f161703897143f
rhalsemd <rhalsemd@naver.com>
2023-04-25 22:06:58 +0900

    [BE] UPDATE : #S08P31D102-167 카카오 지도 주소<-> 위도,경도 변환 테스트및 기능 구현 완료 주소를 통한 위도, 경도 추출 기능 테스트 및 구현 완료 위도, 경도를 통한 주소 추출 기능 테스트 및 구현 완료

--- AMAG_Project/Backend/src/main/java/com/miracle/AMAG/controller/common/AddressController.java
+++ AMAG_Project/Backend/src/main/java/com/miracle/AMAG/controller/common/AddressController.java
@@ -11,10 +11,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.Map;
@@ -27,23 +24,23 @@ public class AddressController {
     @Autowired
     private AddressService addressService;
 
-    @GetMapping("/convert-geo")
+    @GetMapping("/convert-geo/{address}")
     @Operation(description = "주소에 해당하는 지역에 대한 위도, 경도 데이터")
     @Parameters({
             @Parameter(name = "address", description = "주소")
     })
-    public ResponseEntity<?> getGeoAddress(@RequestParam String address) {
+    public ResponseEntity<?> getGeoAddress(@PathVariable("address") String address) {
         Map<String, Object> map = addressService.getGeoAddress(address);
         return NormalResponse.toResponseEntity(HttpStatus.OK, map);
     }
 
-    @GetMapping("/reverse-geo")
+    @GetMapping("/reverse-geo/{lat}/{lng}")
     @Operation(description = "위도, 경도와 일치하는 지역에 대한 지번주소")
     @Parameters({
             @Parameter(name = "lat", description = "주소를 조회할 지역의 위도")
             ,@Parameter(name = "lng", description = "주소를 조회할 지역의 경도")
     })
-    public ResponseEntity<?> getReverseGeo(@RequestParam double lat, @RequestParam double lng) {
+    public ResponseEntity<?> getReverseGeo(@PathVariable("lat") double lat, @PathVariable("lng") double lng) {
         Map<String, Object> map = addressService.getReverseGeo(lat, lng);
         return NormalResponse.toResponseEntity(HttpStatus.OK, map);
     }

01724dd30e02ca61f4d4da8ae541bd8926353d85
miracle3070 <miracle3070@naver.com>
2023-04-26 10:57:01 +0900

    [BE] UPDATE : #S08P31D102-226 API 명세서에 맞게 SMS 인증 컨트롤러 수정

--- AMAG_Project/Backend/src/main/java/com/miracle/AMAG/controller/account/JoinController.java
+++ AMAG_Project/Backend/src/main/java/com/miracle/AMAG/controller/account/JoinController.java
@@ -1,8 +1,10 @@
 package com.miracle.AMAG.controller.account;
 
 import com.miracle.AMAG.dto.requestDTO.AccountRequestDTO;
+import com.miracle.AMAG.entity.common.SmsAuth;
 import com.miracle.AMAG.service.account.JoinService;
 import com.miracle.AMAG.service.common.AddressService;
+import com.miracle.AMAG.service.common.SmsService;
 import com.miracle.AMAG.util.board.BoardUtils;
 import com.miracle.AMAG.util.network.NormalResponse;
 import io.swagger.v3.oas.annotations.tags.Tag;
@@ -13,9 +15,11 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.Random;
+
 @Slf4j
 @Tag(name = "Join", description = "회원가입 API")
-@RequestMapping("/api")
+@RequestMapping("/api/join")
 @RestController
 @RequiredArgsConstructor
 public class JoinController {
@@ -23,9 +27,12 @@ public class JoinController {
     JoinService joinService;
 
     @Autowired
+    SmsService smsService;
+
+    @Autowired
     AddressService addressService;
 
-    @PostMapping("/join")
+    @PostMapping("")
     public ResponseEntity<?> join(@RequestBody AccountRequestDTO dto) {
         joinService.join(dto);
         return NormalResponse.toResponseEntity(HttpStatus.OK, BoardUtils.BOARD_CRUD_SUCCESS);
@@ -43,4 +50,35 @@ public class JoinController {
         }
         return NormalResponse.toResponseEntity(HttpStatus.OK, joinService.checkUserId(id));
     }
+
+    @PostMapping("/sms-send")
+    public ResponseEntity<?> smsAuthSend(@RequestBody SmsAuth smsAuth) throws Exception {
+        String phoneNm = smsAuth.getPhoneNumber();
+        if (phoneNm == null || phoneNm.isBlank()) {
+            throw new NullPointerException("휴대전화 번호를 입력해주세요");
+        }
+
+        String receivePhone = smsAuth.getPhoneNumber();
+        Random random = new Random();
+        StringBuilder authNm = new StringBuilder();
+        for (int i = 0; i < 6; i++) {
+            authNm.append((random.nextInt(10)));
+        }
+
+        String msg = smsService.makeSmsAuthMsg(receivePhone, authNm.toString());
+        smsService.smsAuthSend(smsAuth, msg);
+
+        return NormalResponse.toResponseEntity(HttpStatus.OK, BoardUtils.BOARD_CRUD_SUCCESS);
+    }
+
+    @PostMapping("/sms-auth")
+    public ResponseEntity<?> smsAuth(@RequestBody SmsAuth smsAuth) {
+        String phoneNm = smsAuth.getPhoneNumber();
+        if (phoneNm == null || phoneNm.isBlank()) {
+            throw new NullPointerException("휴대전화 번호를 입력해주세요");
+        }
+
+        return NormalResponse.toResponseEntity(HttpStatus.OK, smsService.smsAuth(smsAuth));
+    }
+
 }

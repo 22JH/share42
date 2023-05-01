c7f91b46b88c488b3013122f44f9eed5268ba014
miracle3070 <miracle3070@naver.com>
2023-05-01 11:04:41 +0900

    [BE] UPDATE : 로그인 시 LoginRequestDTO를  사용하도록 변경

--- AMAG_Project/Backend/src/main/java/com/miracle/AMAG/controller/account/LoginController.java
+++ AMAG_Project/Backend/src/main/java/com/miracle/AMAG/controller/account/LoginController.java
@@ -1,11 +1,15 @@
 package com.miracle.AMAG.controller.account;
 
-import com.miracle.AMAG.entity.account.Account;
+import com.miracle.AMAG.dto.requestDTO.account.LoginRequestDTO;
 import com.miracle.AMAG.service.account.LoginService;
+import com.miracle.AMAG.util.network.CUDResponse;
+import com.miracle.AMAG.util.network.ErrorResponse;
 import com.miracle.AMAG.util.network.NormalResponse;
 import io.swagger.v3.oas.annotations.Operation;
-import io.swagger.v3.oas.annotations.Parameter;
-import io.swagger.v3.oas.annotations.Parameters;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
@@ -23,14 +27,17 @@ public class LoginController {
     @Autowired
     LoginService loginService;
 
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "로그인 성공", content = @Content(schema = @Schema(implementation = CUDResponse.class))),
+            @ApiResponse(responseCode = "500", description = "로그인 실패", content = @Content(schema = @Schema(implementation = ErrorResponse.class))),
+            @ApiResponse(responseCode = "400", description = "잘못된 접근", content = @Content(schema = @Schema(implementation = ErrorResponse.class))),
+            @ApiResponse(responseCode = "404", description = "존재하지 않는 리소스 접근", content = @Content(schema = @Schema(implementation = ErrorResponse.class))),
+            @ApiResponse(responseCode = "405", description = "요청이 잘못되었습니다.", content = @Content(schema = @Schema(implementation = ErrorResponse.class)))})
+    @Operation(summary = "로그인 시도", description = "로그인을 시도합니다.")
+    @io.swagger.v3.oas.annotations.parameters.RequestBody(description = "사용자 로그인 정보", required = true, content = @Content(schema = @Schema(implementation = LoginRequestDTO.class)))
     @PostMapping("/login")
-    @Operation(description = "로그인")
-    @Parameters({
-            @Parameter(name = "id", description = "아이디")
-            ,@Parameter(name = "pwd", description = "비밀번호")
-    })
-    public ResponseEntity<?> login(@RequestBody Account account, HttpServletRequest request) {
-        return NormalResponse.toResponseEntity(HttpStatus.OK, loginService.login(account, request));
+    public ResponseEntity<?> login(@RequestBody LoginRequestDTO dto, HttpServletRequest request) {
+        return NormalResponse.toResponseEntity(HttpStatus.OK, loginService.login(dto, request));
     }
 
     @GetMapping("/logout")

2b1c879fa8313d08c081a29e775af954fa5b6fc0
김지현 <91wlgus@naver.com>
2023-05-03 15:22:45 +0900

    [BE] ADD : #S08P31D102-281 채팅 웹소켓 구현

--- AMAG_Project/Backend/src/main/java/com/miracle/AMAG/dto/chat/ChatMessageDTO.java
+++ AMAG_Project/Backend/src/main/java/com/miracle/AMAG/dto/chat/ChatMessageDTO.java
@@ -1,6 +1,8 @@
 package com.miracle.AMAG.dto.chat;
 
+import jakarta.persistence.Id;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 import org.springframework.data.mongodb.core.mapping.Document;
 
@@ -8,18 +10,34 @@ import java.time.LocalDateTime;
 
 @Getter
 @Setter
+@NoArgsConstructor
 @Document(collection = "share-chat")
 public class ChatMessageDTO {
+    @Id
+    private String id;
     private String sender;
+    private String receiver;
     private String content;
-    private LocalDateTime createdAt;
+//    private LocalDateTime createdAt;
+    private String createdAt;
     private String roomName; // 방이름
 
-    public ChatMessageDTO(String sender, String content, String roomName) {
+    public ChatMessageDTO(String sender, String receiver, String content, String roomName) {
         this.sender = sender;
+        this.receiver = receiver;
         this.content = content;
-        this.createdAt = LocalDateTime.now();
+        this.createdAt = LocalDateTime.now().toString();
         this.roomName = roomName;
     }
 
+    @Override
+    public String toString() {
+        return "{" +
+                "sender='" + sender + '\'' +
+                ", receiver='" + receiver + '\'' +
+                ", content='" + content + '\'' +
+                ", createdAt='" + createdAt + '\'' +
+                ", roomName='" + roomName + '\'' +
+                '}';
+    }
 }
--- AMAG_Project/Backend/src/main/java/com/miracle/AMAG/handler/chat/ChatWebSocketHandler.java
+++ AMAG_Project/Backend/src/main/java/com/miracle/AMAG/handler/chat/ChatWebSocketHandler.java
@@ -1,6 +1,8 @@
 package com.miracle.AMAG.handler.chat;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.miracle.AMAG.dto.chat.ChatMessageDTO;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.stereotype.Component;
@@ -12,26 +14,96 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.ConcurrentHashMap;
 
+@Slf4j
 @Component
 public class ChatWebSocketHandler implements WebSocketHandler {
+    @Autowired
+    private MongoTemplate mongoTemplate;
+    private List<WebSocketSession> sessions = new ArrayList<>();
+    private ConcurrentHashMap<String, List<WebSocketSession> > sessionToRoomMap = new ConcurrentHashMap<>();
+//    private ConcurrentHashMap<String, List<Account> > sessionToRoomMap = new ConcurrentHashMap<>();
+//    private ConcurrentHashMap<Account, WebSocketSession> userSocket = new ConcurrentHashMap<>();
+    private String getRoomNameFromUri(URI uri) {
+        // uri에서 room정보를 받는거
+        String path = uri.getPath();
+        String[] parts = path.split("/");
+        log.info("uri에 접근이 됬는데 어디냐면"+parts[parts.length - 1]);
+        return parts[parts.length - 1];
+    }
+
 
     @Override
     public void afterConnectionEstablished(WebSocketSession session) throws Exception {
+        // 채팅방 이름을 가져옴
+        String roomName = getRoomNameFromUri(session.getUri());
+        // WebSocket 세션과 채팅방을 매핑시킴
+        if( sessionToRoomMap.get(roomName) == null ) {
+//            List<WebSocketSession> list = new ArrayList<>();
+            sessions.add(session);
+//            list.add(session);
+            sessionToRoomMap.put(roomName, sessions);
+            log.info("새로운 방생성, session은" + session);
 
+        } else {
+            System.out.println("기존의 방 접근");
+            List<WebSocketSession> sessionsInRoom = sessionToRoomMap.get(roomName);
+            // 이미 종료된 WebSocket 세션은 제거
+            sessionsInRoom.removeIf(s -> !s.isOpen());
+            // 새로운 WebSocket 세션을 추가
+            sessions.add(session);
+            sessionToRoomMap.put(roomName, sessions);
+//            sessionsInRoom.add(session);
+
+        }
+
+
+        // 채팅방에 입장한 사용자에게 이전 대화 내용을 전송
+        List<ChatMessageDTO> chatMessages = mongoTemplate.findAll(ChatMessageDTO.class);
+//        chatMessages.stream()
+//                .filter(chatMessageDTO -> chatMessageDTO.getRoomName().equals(roomName))
+//                .forEach(chatMessageDTO -> {
+//                    String payload = chatMessageDTO.toString();
+//                    TextMessage textMessage = new TextMessage(payload);
+//                    session.sendMessage(textMessage);
+//                });
+        for (ChatMessageDTO chatMessage : chatMessages) {
+            if (chatMessage.getRoomName().equals(roomName)) {
+                String payload = chatMessage.toString();
+                TextMessage textMessage = new TextMessage(payload);
+                session.sendMessage(textMessage);
+            }
+        }
     }
 
     @Override
     public void handleMessage(WebSocketSession session, WebSocketMessage<?> message) throws Exception {
-
+        // 클라이언트로부터 메시지를 수신할 때 실행되는 메서드(보낼때)
+        String payload = (String) message.getPayload();
+        //db저장(프론트에서 소켓payload로 준 데이터를 set해서 저장)
+        ObjectMapper objectMapper = new ObjectMapper();
+        ChatMessageDTO tmp = objectMapper.readValue(payload, ChatMessageDTO.class);
+        tmp.setCreatedAt(LocalDateTime.now().toString());
+        mongoTemplate.save(tmp);
+        //소켓send
+        TextMessage textMessage = new TextMessage(payload);
+        for (WebSocketSession webSocketSession : sessionToRoomMap.get(tmp.getRoomName())) {
+            webSocketSession.sendMessage(textMessage);
+            log.info("textMessage{}"+textMessage);
+            break;
+        }
     }
     @Override
     public void handleTransportError(WebSocketSession session, Throwable exception) throws Exception {
-
+        // WebSocket 통신 중 에러가 발생할 때 실행되는 메서드
+        System.out.println("error");
+        log.debug("error{}"+exception);
     }
 
     @Override
     public void afterConnectionClosed(WebSocketSession session, CloseStatus closeStatus) throws Exception {
-
+        // 클라이언트와 연결이 종료될 때 실행되는 메서드
+        sessions.remove(session);
+        log.info("연결이 종료됬다");
     }
 
     @Override

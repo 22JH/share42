55b73c8919c9865645e1f121de0e49e615c27ebc
ggnsall <ggnsall@naver.com>
2023-05-03 13:38:27 +0900

    [FE] UPDATE : #S08P31D102-74 관리자 페이지 차트 스켈레톤 코드 완료 - arc 차트 CSS 수정

--- AMAG_Project/Frontend/ou42/src/components/admin/home/AdminHomeCircleChart.tsx
+++ AMAG_Project/Frontend/ou42/src/components/admin/home/AdminHomeCircleChart.tsx
@@ -5,25 +5,38 @@ import { useEffect, useRef } from "react";
 import { arc, interpolate, pie, select } from "d3";
 
 const data = [
-  [50, "red"],
-  [150, "orange"],
-  [100, "green"],
-  [200, "blue"],
+  [50, "red", "전국"],
+  [150, "orange", "서울"],
+  [100, "green", "대전"],
+  [200, "tomato", "인천"],
+  [250, "blue", "대구"],
+  [30, "purple", "광주"],
+  [170, "black", "부산"],
+  [230, "lightgreen", "울산"],
 ];
 
 const info = css`
   display: flex;
-  justify-content: center;
+  width: 100%;
   margin-bottom: 10%;
+  justify-content: center;
   .container {
     display: flex;
     align-items: center;
+    padding-top: 3%;
     p {
       margin: 0;
       padding-left: 1.5vw;
       font-size: 0.9rem;
     }
   }
+`;
+
+const outer = css`
+  width: 70%;
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: center;
 `;
 
 const box = (d: any[]) => css`
@@ -35,7 +48,11 @@ const box = (d: any[]) => css`
 
 const DURATION: 1000 = 1000;
 
-function AdminHomeChart() {
+function AdminHomeCircleChart({
+  setChange,
+}: {
+  setChange: React.Dispatch<React.SetStateAction<boolean>>;
+}) {
   const canvas = useRef<HTMLDivElement>(null);
 
   useEffect((): any => {
@@ -45,12 +62,12 @@ function AdminHomeChart() {
     const svg = canv.append("svg").attr("width", "100%").attr("height", "32vh");
     // .style("padding-left", "8%");
 
-    const g = svg.append("g").attr("transform", "translate(195, 120)");
+    const g = svg.append("g").attr("transform", "translate(195, 130)");
 
     const f: any = arc()
-      .innerRadius(70)
+      .innerRadius(55)
       .outerRadius(120)
-      .padAngle(0.03)
+      .padAngle(0.025)
       .cornerRadius(5);
 
     const pieGraph: any = pie()
@@ -66,6 +83,9 @@ function AdminHomeChart() {
       })
       .attr("stroke-width", "2px")
       .attr("d", f)
+      .on("click", (event: any, d: any) => {
+        setChange((isChange) => !isChange);
+      }) // 이 부분을 추가합니다.
       .transition()
       .duration(DURATION)
       .attrTween("d", (d: any) => {
@@ -79,31 +99,40 @@ function AdminHomeChart() {
       .append("text")
       .attr(
         "transform",
-        (d: any) => `translate(${f.centroid(d)[0] - 13}, ${f.centroid(d)[1]})`
+        (d: any) => `translate(${f.centroid(d)[0] - 10}, ${f.centroid(d)[1]})`
       )
       .attr("fill", "white")
       .attr("dy", "0.35em")
-      .text((d: any) => d.data[0])
+      .attr("font-size", "0.8rem")
+      .attr("font-weight", "900")
+      .text((d: any) => {
+        if (d.data[0] >= 100) {
+          return d.data[2];
+        }
+        return;
+      })
       .transition()
       .duration(DURATION)
       .attr("fill", "white");
   }, []);
 
   return (
-    <>
+    <div>
       <div className="canvas" ref={canvas}></div>
       <div css={info}>
-        {data.map((d: any[], index: number) => {
-          return (
-            <div className="container">
-              <div css={box(d)}></div>
-              <p>{d[0]}</p>
-            </div>
-          );
-        })}
+        <div css={outer}>
+          {data.map((d: any[], index: number) => {
+            return (
+              <div className="container" key={index}>
+                <div css={box(d)}></div>
+                <p>{d[2]}</p>
+              </div>
+            );
+          })}
+        </div>
       </div>
-    </>
+    </div>
   );
 }
 
-export default AdminHomeChart;
+export default AdminHomeCircleChart;

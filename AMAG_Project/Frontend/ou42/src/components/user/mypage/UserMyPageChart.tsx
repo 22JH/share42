24262d84fe4132568e31539a46cbb62637416667
ggnsall <ggnsall@naver.com>
2023-05-04 16:31:47 +0900

    [FE] UPDATE : 유저 마이페이지 line 차트 추가

--- AMAG_Project/Frontend/ou42/src/components/user/mypage/UserMyPageChart.tsx
+++ AMAG_Project/Frontend/ou42/src/components/user/mypage/UserMyPageChart.tsx
@@ -1,43 +1,117 @@
 /** @jsxImportSource @emotion/react */
 import { css } from "@emotion/react";
 
-import { select } from "d3";
-import { useEffect } from "react";
+import {
+  axisBottom,
+  axisLeft,
+  line,
+  max,
+  scaleBand,
+  scaleLinear,
+  curveBasis,
+  select,
+} from "d3";
+import { useEffect, useRef } from "react";
 
-const chartStyle = css``;
+import { data } from "../../SampleData";
 
-const sampleData = [200, 150, 30, 100, 300, 240, 270, 70, 10];
+const chartStyle = css`
+  height: 100%;
+  .canvas {
+    overflow-x: auto;
+    &::-webkit-scrollbar {
+      display: none;
+    }
+  }
+
+  .line-path {
+    fill: none;
+    stroke: steelblue;
+  }
+`;
 
 function UserMyPageChart() {
+  const canvas = useRef<HTMLDivElement>(null);
+
   useEffect(() => {
-    const width: 100 = 100;
-    const height: 40 = 40;
-    const svg = select(".canvas");
+    const canv = select(canvas.current);
+    canv.selectAll("*").remove();
 
-    svg.selectAll("*").remove();
+    const Y = 250;
+    const svgWidth = data.length * 50 + 100;
 
-    const frame = svg
+    const svg = canv
       .append("svg")
-      .attr("width", `${width}vw`)
-      .attr("height", `${height}vh`);
+      .attr("width", svgWidth)
+      .attr("height", "37vh");
 
-    const graph1 = frame
+    const g = svg
       .append("g")
-      .attr("width", "auto")
-      .attr("height", "auto");
+      .attr("width", "100%")
+      .attr("transform", "translate(45, 50)");
 
-    sampleData.forEach((data, index) => {
-      graph1
-        .append("rect")
-        .attr("height", data)
-        .attr("width", 30)
-        .attr("x", 40 * index)
-        .attr("y", 350 - data);
-    });
+    const xAxisG = g.append("g").attr("transform", `translate(0, ${Y})`);
+    const yAxisG = g.append("g");
+
+    const x: any = scaleBand()
+      .domain(data.map((item) => item["지역이름"]))
+      .range([0, svgWidth - 100])
+      .padding(0.2);
+
+    const y: any = scaleLinear()
+      .domain([0, max(data, (d) => d["확진자수"])])
+      .range([Y, 0]);
+
+    const LINE = line()
+      .x((d: any) => x(d.지역이름) + 20)
+      .y((d: any) => y(d.확진자수) - 10)
+      .curve(curveBasis);
+
+    const lines = g.selectAll("path").data(data);
+
+    lines
+      .enter()
+      .append("path")
+      .transition()
+      .duration(2000)
+      .attr("fill", "none")
+      .attr("stroke", "steelblue")
+      .attr("stroke-width", "2px")
+      .attr("stroke-dasharray", function () {
+        const pathLength: any = this.getTotalLength();
+        return `${pathLength} ${pathLength}`;
+      })
+      .attr("stroke-dashoffset", function () {
+        const pathLength: any = this.getTotalLength();
+        return `${pathLength}`;
+      })
+
+      .attr("stroke-dashoffset", 0)
+      .attr("d", LINE(data));
+    // .enter()
+    // .append("path")
+    // .attr("fill", "none")
+    // .attr("stroke", "blue")
+    // .attr("stroke-width", "2px")
+    // .transition()
+    // .duration(1000)
+    // .attr("d", LINE(data));
+
+    const xAxis = axisBottom(x).tickSizeInner(-Y).tickPadding(15);
+    const yAxis = axisLeft(y)
+      .tickSizeInner(-(svgWidth - 100))
+      .tickPadding(10);
+
+    xAxisG.call(xAxis);
+    yAxisG.call(yAxis);
+
+    xAxisG.selectAll(".tick line").attr("opacity", 0.3);
+    yAxisG.selectAll(".tick line").attr("opacity", 0.3);
   }, []);
+
   return (
     <div css={chartStyle}>
-      <div className="canvas"></div>
+      <div className="canvas" ref={canvas}></div>
     </div>
   );
 }

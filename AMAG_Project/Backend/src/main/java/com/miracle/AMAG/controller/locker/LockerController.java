3dd9fa5d667288504c63f99c53f66a6a683c6638
rhalsemd <rhalsemd@naver.com>
2023-05-08 16:42:20 +0900

    [BE] ADD : 선택한 공유함의 칸별 상세 정보

--- AMAG_Project/Backend/src/main/java/com/miracle/AMAG/controller/locker/LockerController.java
+++ AMAG_Project/Backend/src/main/java/com/miracle/AMAG/controller/locker/LockerController.java
@@ -2,7 +2,7 @@ package com.miracle.AMAG.controller.locker;
 
 import com.miracle.AMAG.service.common.AddressService;
 import com.miracle.AMAG.service.locker.LockerService;
-import com.miracle.AMAG.util.network.CUDResponse;
+import com.miracle.AMAG.util.board.BoardUtils;
 import com.miracle.AMAG.util.network.ErrorResponse;
 import com.miracle.AMAG.util.network.NormalResponse;
 import io.swagger.v3.oas.annotations.Operation;
@@ -17,6 +17,7 @@ import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -52,5 +53,24 @@ public class LockerController {
         Map<String, Object> map = addressService.getReverseGeo(lat, lng);
         return NormalResponse.toResponseEntity(HttpStatus.OK, lockerService.getLocker(map.get("region_1depth_name").toString()
                 , map.get("region_2depth_name").toString(), map.get("region_3depth_name").toString()));
+    }
+
+    @GetMapping("/detail/{page}/{size}/{lockerLocationId}")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "대여함 상세 조회 성공", content = @Content(schema = @Schema(implementation = NormalResponse.class))),
+            @ApiResponse(responseCode = "500", description = "대여함 상세 조회 실패", content = @Content(schema = @Schema(implementation = ErrorResponse.class))),
+            @ApiResponse(responseCode = "400", description = "잘못된 접근", content = @Content(schema = @Schema(implementation = ErrorResponse.class))),
+            @ApiResponse(responseCode = "404", description = "존재하지 않는 리소스 접근", content = @Content(schema = @Schema(implementation = ErrorResponse.class))),
+            @ApiResponse(responseCode = "405", description = "요청이 잘못되었습니다.", content = @Content(schema = @Schema(implementation = ErrorResponse.class)))})
+    @Operation(summary = "대여함 칸 정보 상세 조회", description = "대여함 Id에 해당하는 대여함 칸들의 정보를 조회합니다.")
+    @Parameters({
+            @Parameter(name = "page", description = "공유함 칸 페이지 번호", in = ParameterIn.PATH),
+            @Parameter(name = "size", description = "페이지 당 공유함 칸 수", in = ParameterIn.PATH),
+            @Parameter(name = "lockerLocationId", description = "공유함 번호", in = ParameterIn.PATH)
+    })
+    public ResponseEntity<?> getLockerDetail(@PathVariable("page") int page, @PathVariable("size") int size,
+                                             @PathVariable("lockerLocationId") int id) {
+        PageRequest pageRequest = BoardUtils.pageRequestInit(page, size, "id" , BoardUtils.ORDER_BY_DESC);
+        return NormalResponse.toResponseEntity(HttpStatus.OK, lockerService.getLockerDetail(id,pageRequest));
     }
 }
--- AMAG_Project/Backend/src/main/java/com/miracle/AMAG/service/locker/LockerService.java
+++ AMAG_Project/Backend/src/main/java/com/miracle/AMAG/service/locker/LockerService.java
@@ -1,6 +1,6 @@
 package com.miracle.AMAG.service.locker;
 
-import com.miracle.AMAG.entity.locker.LockerStation;
+import com.miracle.AMAG.mapping.locker.LockerGetListMapping;
 import com.miracle.AMAG.mapping.locker.LockerStationGetListMapping;
 import com.miracle.AMAG.repository.locker.LockerRepository;
 import com.miracle.AMAG.repository.locker.LockerStationRepository;
@@ -8,9 +8,12 @@ import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @Slf4j
 @Service
@@ -21,9 +24,24 @@ public class LockerService {
     @Autowired
     private LockerStationRepository lockerStationRepository;
 
+    @Autowired
+    private LockerRepository lockerRepository;
+
     public List<LockerStationGetListMapping> getLocker(String sido, String sigungu, String dong){
         List<LockerStationGetListMapping> lsglm = lockerStationRepository.findAllBySidoAndSigunguAndDong(sido, sigungu, dong);
         return lsglm;
+    }
+
+    public Map<String, Object> getLockerDetail(int id, Pageable pageable){
+        Long totalCount = lockerRepository.countByLockerStation_Id(id);
+        List<LockerGetListMapping> lglm = lockerRepository.findAllByLockerStation_IdAndShareArticleNotNull(id, pageable);
+        int useCount = lglm.size();
+
+        Map<String, Object> result = new HashMap<>();
+        result.put("totalCount", totalCount);
+        result.put("useCount", totalCount-useCount);
+        result.put("lockerList", lglm);
+        return result;
     }
 
 }

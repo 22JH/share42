4ba1c59050c68c0048f5f5a4502918c12150f30b
김지현 <91wlgus@naver.com>
2023-05-08 14:26:41 +0900

    [BE] UPDATE: '지짐' ->'지점', 'categoty'->'category' 단어수정, 불필요한 import삭제

--- AMAG_Project/Backend/src/main/java/com/miracle/AMAG/service/admin/AdminReportService.java
+++ AMAG_Project/Backend/src/main/java/com/miracle/AMAG/service/admin/AdminReportService.java
@@ -2,11 +2,9 @@ package com.miracle.AMAG.service.admin;
 
 import com.miracle.AMAG.config.SecurityUtil;
 import com.miracle.AMAG.entity.account.Account;
-import com.miracle.AMAG.entity.user.ShareArticle;
 import com.miracle.AMAG.mapping.admin.ReportListMapping;
 import com.miracle.AMAG.repository.account.AccountRepository;
 import com.miracle.AMAG.repository.user.ReportRepository;
-import com.miracle.AMAG.util.common.ShareArticleUtils;
 import jakarta.transaction.Transactional;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -24,7 +22,7 @@ public class AdminReportService {
     @Autowired
     private AccountRepository accountRepository;
 
-    public Page<ReportListMapping> getReportList(int categoty, PageRequest pageRequest){
+    public Page<ReportListMapping> getReportList(int category, PageRequest pageRequest){
         String loginId = SecurityUtil.getCurrentUserId();
         Account account = accountRepository.findByUserId(loginId);
         if(account == null){
@@ -33,7 +31,7 @@ public class AdminReportService {
         if (!account.getRole().value().equals("ROLE_ADMIN")){
             throw new RuntimeException("권한이 없습니다");
         }
-        Page<ReportListMapping> result = reportRepository.findAllByCategoryOrderByIdDesc(categoty,pageRequest);
+        Page<ReportListMapping> result = reportRepository.findAllByCategoryOrderByIdDesc(category,pageRequest);
 
         return result;
     }

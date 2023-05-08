1ed6ca8ff1a1f0de94c2a142763df01e3cf425b8
rhalsemd <rhalsemd@naver.com>
2023-05-08 20:34:24 +0900

    [BE] UPDATE : 공유함 현재 위치 기반 조회 기능 수정 좌표를 통한 주소로 조회 -> 좌표를 통한 현재 위치에서 10km 이내의 공유함 위치

--- AMAG_Project/Backend/src/main/java/com/miracle/AMAG/repository/locker/LockerStationRepository.java
+++ AMAG_Project/Backend/src/main/java/com/miracle/AMAG/repository/locker/LockerStationRepository.java
@@ -4,6 +4,7 @@ import com.miracle.AMAG.entity.locker.LockerStation;
 import com.miracle.AMAG.mapping.locker.LockerStationGetListMapping;
 import io.lettuce.core.dynamic.annotation.Param;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -11,6 +12,15 @@ import java.util.List;
 @Repository
 public interface LockerStationRepository extends JpaRepository<LockerStation, Integer> {
 
-    List<LockerStationGetListMapping> findAllBySidoAndSigunguAndDong(@Param("sido") String sido, @Param("sigungu") String sigungu,
-                                                                     @Param("dong") String dong);
+    @Query(value = """
+    SELECT ls
+            FROM LockerStation ls
+            WHERE 6371 * 2 * ASIN(SQRT(
+    POWER(SIN((:lat - ABS(ls.lat)) * PI() / 180 / 2), 2) +\s
+    COS(:lat * PI() / 180) * COS(ABS(ls.lat) * PI() / 180) *\s
+    POWER(SIN((:lng - ls.lng) * PI() / 180 / 2), 2)
+)
+            ) <= 10
+""")
+    List<LockerStationGetListMapping> getNearLocker(@Param("lat") double lat, @Param("lng") double lng);
 }

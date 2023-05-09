bdc4c71b5c3205c13aeb36fdd1ecf23d6af90323
rhalsemd <rhalsemd@naver.com>
2023-05-09 15:20:48 +0900

    [BE] UPDATE : 공유 물품 글 등록 부분 수정 보관처리 된 사진 및 반납 처리 진행된 사진 목록 조회 기능 추가

--- AMAG_Project/Backend/src/main/java/com/miracle/AMAG/repository/user/KeepRepository.java
+++ AMAG_Project/Backend/src/main/java/com/miracle/AMAG/repository/user/KeepRepository.java
@@ -1,9 +1,13 @@
 package com.miracle.AMAG.repository.user;
 
 import com.miracle.AMAG.entity.user.Keep;
+import com.miracle.AMAG.mapping.user.KeepGetImgMapping;
+import io.lettuce.core.dynamic.annotation.Param;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface KeepRepository extends JpaRepository<Keep, Integer> {
+
+    KeepGetImgMapping findByShareArticle_Id(@Param("shareArticleId") int shareArticleId);
 }

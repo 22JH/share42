04efd44942478ec7c63893f67103128ad0393062
tmdwns950428 <tmdwns950428@naver.com>
2023-05-15 16:00:53 +0900

    [FE]:ADD #S08P31D102-412 회수 신청 안내 모달 및 회수 신청 API 연동 완성 - style, type 정리 아직 안함 - 회수 신청 안내 모달 완성 (dialog)

--- AMAG_Project/Frontend/ou42/src/components/sharedetail/UserShareDetailRequest.tsx
+++ AMAG_Project/Frontend/ou42/src/components/sharedetail/UserShareDetailRequest.tsx
@@ -3,6 +3,7 @@ import { css } from "@emotion/react";
 import axios from "axios";
 import { useEffect, useRef, useState } from "react";
 import { useNavigate, useParams } from "react-router-dom";
+import swal from "sweetalert";
 import { UserShareDetailRequestProps } from "./type/UserShareDetailType";
 
 export const dialog = css`
@@ -33,6 +34,14 @@ const SHARE_USE_API = () => {
   return `https://www.share42-together.com/api/common/usage/1`;
 };
 
+const COLLECT_USE_API = () => {
+  return `https://www.share42-together.com/api/common/usage/3`;
+};
+
+const USER_COLLECT_API = (id : string | undefined) => {
+  return `https://www.share42-together.com/api/user/share/collect/${id}`
+}
+
 const UserShareDetailRequest = ({
   useRequest,
   handleUseRequest,
@@ -45,8 +54,10 @@ const UserShareDetailRequest = ({
   const navigate = useNavigate();
   const loginObject = localStorage.getItem("loginInfo");
   const { token } = loginObject ? JSON.parse(loginObject) : null;
+  const { userId } = loginObject ? JSON.parse(loginObject) : null;
   const dialogRef = useRef<HTMLDialogElement | any>({});
   const [termsContent, setTermsContent] = useState<string[]>([]);
+  const [collectContent, setCollectContent] = useState<string[]>([]);
 
   useEffect(() => {
     axios({
@@ -63,6 +74,43 @@ const UserShareDetailRequest = ({
       .catch((e) => console.log(e));
   }, []);
 
+  useEffect(() => {
+    axios({
+      method: "GET",
+      url: COLLECT_USE_API(),
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    })
+      .then((res: any) => {
+        const lst = res.data.message[0].content.split("\r\n");
+        setCollectContent(lst.slice(0, lst.length - 1));
+      })
+      .catch((e) => console.log(e));
+  }, []);
+
+  const handleUserCollect = (id:string | undefined) => {
+    axios({
+      method: 'POST',
+      url: USER_COLLECT_API(id),
+      headers: {
+        Authorization: `Bearer ${token}`,
+      }
+    })
+    .then((res) => {
+      if (res.data.status === 200) {
+        swal("신청 성공", "회수 신청이 완료되었습니다.", "success");
+        navigate('/user/nfc')
+      } else {
+        swal("신청 실패", "회수 신청이 실패되었습니다.", "error");
+      }
+      return res.data.message
+    })
+    .catch((e) => {
+      console.log(e)
+      swal("서버 오류", "서버 오류로 신청이 실패되었습니다.", "error");
+    })
+  }
 
   return (
     <div
@@ -92,7 +140,7 @@ const UserShareDetailRequest = ({
           {data?.article.accountSigungu} {data?.article.accountDong}
         </span>
       </div>
-      <div>
+      {userId !== data?.article.accountUserId ? <div>
         {useRequest ? (
           <>
             <button
@@ -158,7 +206,22 @@ const UserShareDetailRequest = ({
           >
           채팅하기
         </button>
-      </div>
+      </div> : data?.article.shareStatus === 1 ? <button
+          style={{
+            marginLeft: "3vw",
+            color: "#ffffff",
+            backgroundColor: "#1f7500",
+            border: "none",
+            borderRadius: "5px",
+            boxShadow: "2px 2px 5px #00000051",
+            padding: "1.4vh 4vw",
+          }}
+          onClick={() => {
+            dialogRef?.current.showModal();
+          }}
+          >
+          회수하기
+        </button> : null}
       <dialog
         ref={(ref) => {
           return (dialogRef.current = ref);
@@ -188,14 +251,14 @@ const UserShareDetailRequest = ({
             textAlign: 'left',
             paddingLeft: '20px'
           }}
-        >
+          >
           {termsContent.map((term, index) => (
             <li 
             style={{
-                listStyleType: 'dicimal'
-              }}
-              key={index}>{term}</li>
-          ))}
+              listStyleType: 'dicimal'
+            }}
+            key={index}>{term}</li>
+            ))}
         </ul>
         <button
           style={{
@@ -209,6 +272,58 @@ const UserShareDetailRequest = ({
           onClick={() => handleUseRequest(id)}
           >
           사용 신청하기
+        </button>
+      </dialog>
+      <dialog
+        ref={(ref) => {
+          return (dialogRef.current = ref);
+        }}
+        css={dialog}
+        style={{
+          textAlign: 'center'
+        }}
+        >
+        <h1>HOW TO 회수신청?</h1>
+        <button
+          onClick={() => {
+            (dialogRef.current as any).close();
+          }}
+          style={{
+            position: 'absolute',
+            top: '20px',
+            right: '20px',
+            border: 'none',
+            backgroundColor: '#ffffff'
+          }}
+        >
+          X
+        </button>
+        <ul
+          style={{
+            textAlign: 'left',
+            paddingLeft: '20px'
+          }}
+          >
+          {collectContent.map((term, index) => (
+            <li 
+              style={{
+                listStyleType: 'dicimal'
+              }}
+            key={index}>{term}</li>
+          ))}
+        </ul>
+        <button
+          style={{
+            padding: '3% 6%',
+            fontWeight: '900',
+            color: '#ffffff',
+            backgroundColor: '#FFABAB',
+            border: 'none',
+            borderRadius: '12px'
+          }}
+          onClick={() => handleUserCollect(id)}
+          >
+          회수 신청하기
         </button>
       </dialog>
     </div>

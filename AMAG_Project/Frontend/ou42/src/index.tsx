884818f1170a7f09061ca520cc15efc59b678129
miracle3070 <miracle3070@naver.com>
2023-05-24 13:30:42 +0900

    [ETC] DELETE : 중요 정보 삭제

--- AMAG_Project/Frontend/ou42/src/index.tsx
+++ AMAG_Project/Frontend/ou42/src/index.tsx
@@ -8,15 +8,6 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 // https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
-const firebaseConfig = {
-  apiKey: "AIzaSyADoJ1F-rpXj_T_V714GOOJd6TZigl0y8s",
-  authDomain: "ou42-ce038.firebaseapp.com",
-  projectId: "ou42-ce038",
-  storageBucket: "ou42-ce038.appspot.com",
-  messagingSenderId: "902867957",
-  appId: "1:902867957:web:9d2a8c7eaa58ef885e4ebc",
-};
 
 // Initialize Firebase
 export const app = initializeApp(firebaseConfig);
